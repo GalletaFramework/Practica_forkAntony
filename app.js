5d72ba9335e771d472bb2828668a2320dc4a06e9
Antony <antonyquisperodas@gmail.com>
2026-05-20 22:49:19 -0500

    feat: guardar ticket en localStorage

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ const formulario = document.getElementById("formulario");
 
 formulario.addEventListener("submit", function procesarTicket(e) {
     e.preventDefault();
+    
+    const nuevoTicket = { id: 1, usuario: "Antony", estado: "Abierto" };
+    localStorage.setItem("ticket_reciente", JSON.stringify(nuevoTicket));
 });
